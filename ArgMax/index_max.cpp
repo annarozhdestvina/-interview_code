33eef98229597c5df2f4f16a9afdbb755f038e16
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-28 13:39:45 +0300

    min row and col

--- ArgMax/index_max.cpp
+++ ArgMax/index_max.cpp
@@ -6,8 +6,8 @@ std::pair<size_t, size_t> MatrixArgMax(const std::vector<std::vector<int> >& mat
     size_t min_row = 0;
     size_t min_col = 0;
 
-    for(int i = 0; i < matrix.size(); i++) {
-        for(int j = 0; j < matrix[i].size(); j++) {
+    for(int i = 0; i < static_cast<int>(matrix.size()); i++) {
+        for(int j = 0; j < static_cast<int>(matrix[i].size()); j++) {
             if(l < matrix[i][j]) {
                 l = matrix[i][j];
                 min_row = i;
@@ -25,8 +25,8 @@ int main() {
     std::cin >> n;
     std::cin >> m;
 
-    const int rows = n;
-    const int cols = m;
+    // const int rows = n;
+    // const int cols = m;
 
 
     const std::vector<int> m1(m);
@@ -34,16 +34,17 @@ int main() {
 
     int k = 1;
 
-    for(int i = 0; i < matrix.size(); i++) 
-        for(int j = 0; j < matrix[i].size(); j++) {
+    for(int i = 0; i < static_cast<int>(matrix.size()); i++) 
+        for(int j = 0; j < static_cast<int>(matrix[i].size()); j++) {
             matrix[i][j] = k;
             k++;
         }
 
     matrix[0][2] = 77;
+    matrix[0][3] = 77;
 
-    for(int i = 0; i < matrix.size(); i++) {
-        for(int j = 0; j < matrix[i].size(); j++) {
+    for(int i = 0; i < static_cast<int>(matrix.size()); i++) {
+        for(int j = 0; j < static_cast<int>(matrix[i].size()); j++) {
             std::cout << matrix[i][j] << ' ';
         }
         std::cout << '\n';
